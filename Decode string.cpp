675a62294804cec870c4b309da145bb92f56086b
Baslael Workineh <64444969+BaslaelWorkineh@users.noreply.github.com>
2023-02-01 18:34:50 +0300

    Create Decode string.cpp

---
+++ Decode string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    string decodeString(string& s, int& i) {
+        string result;
+        while(i < s.length() && s[i] != ']')
+        {
+            if(isdigit(s[i]))
+            {
+                int k = 0;
+                while(i < s.length() && isdigit(s[i])) 
+                     k = k*10 + s[i++] -'0';
+                i++;
+                string r = decodeString(s, i);
+                while(k-- > 0)
+                {
+                    result += r;
+                }
+                i++;
+            }
+            else
+                result += s[i++];
+        }
+        return result;
+    }
+public:
+    string decodeString(string s) {
+        int i =0;
+        return decodeString(s, i);
+    }
+};
